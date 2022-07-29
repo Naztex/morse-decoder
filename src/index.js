52cf53d1f07cf758edd7078f54c5b58b92d72d1f
Naztex <nazteck@yandex.ru>
2022-07-29 16:50:55 +0300

    feat: morse decoder function

--- src/index.js
+++ src/index.js
@@ -35,10 +35,15 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+	 '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+	 let elements = expr.match(/.{10}/g);
+	 let elementsReplace = elements.map(element => element.replace(/10/g, '.').replace(/11/g, '-').replace(/00/g, ''));
+	 let elementsNewValue = elementsReplace.map(element => element = MORSE_TABLE[element]);
+	 let elementsNewString = elementsNewValue.join('');
+	 return elementsNewString;
 }
 
 module.exports = {
